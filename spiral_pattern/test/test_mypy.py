62c2ffdd623b358400b5d407a02cf7950099ede7
JoeCode00 <joe.t.hill00@gmail.com>
2026-04-01 14:56:51 -0400

    ignoring wrong import error in a test

--- spiral_pattern/test/test_mypy.py
+++ spiral_pattern/test/test_mypy.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ament_mypy.main import main
+from ament_mypy.main import main # type: ignore
 import pytest
 
 
