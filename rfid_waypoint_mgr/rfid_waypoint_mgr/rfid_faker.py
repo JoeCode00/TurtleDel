0426a92cb1c1b4c5039b72ad68a5a22b9df50d43
JoeCode00 <joe.t.hill00@gmail.com>
2026-04-14 16:35:29 -0400

    rfid_faker as a template for the output of rfid_finder

--- rfid_waypoint_mgr/rfid_waypoint_mgr/rfid_faker.py
+++ rfid_waypoint_mgr/rfid_waypoint_mgr/rfid_faker.py
@@ -3,7 +3,7 @@
 import rclpy
 from rclpy.node import Node
 import tf2_ros
-from geometry_msgs.msg import TransformStamped, PoseStamped
+from geometry_msgs.msg import TransformStamped, PoseWithCovarianceStamped
 
 node_name = 'rfid_faker_node'
 qos = 10
@@ -15,32 +15,32 @@ class rfid_faker_node_class(Node):
 
         self.tf_broadcaster = tf2_ros.TransformBroadcaster(self)
 
-        self.rfid_frames: dict[str, PoseStamped] = {
+        self.rfid_pose: dict[str, PoseWithCovarianceStamped] = {
             'rfid0': self.make_pose(-1.0, 0.0, 0.0),
             'rfid1': self.make_pose(-2.0, 0.0, 0.0),
         }
 
-        self.create_timer(0.1, self.broadcast_frames)
+        self.create_timer(0.1, self.broadcast_pose)
 
-    def make_pose(self, x: float, y: float, z: float) -> PoseStamped:
-        pose = PoseStamped()
-        pose.header.frame_id = 'map'
-        pose.pose.position.x = x
-        pose.pose.position.y = y
-        pose.pose.position.z = z
-        pose.pose.orientation.w = 1.0
-        return pose
+    def make_pose(self, x: float, y: float, z: float) -> PoseWithCovarianceStamped:
+        rfid_pose = PoseWithCovarianceStamped()
+        rfid_pose.header.frame_id = 'map'
+        rfid_pose.pose.pose.position.x = x
+        rfid_pose.pose.pose.position.y = y
+        rfid_pose.pose.pose.position.z = z
+        rfid_pose.pose.pose.orientation.w = 1.0
+        return rfid_pose
 
-    def broadcast_frames(self):
-        for frame_id, pose in self.rfid_frames.items():
+    def broadcast_pose(self):
+        for frame_id, pose in self.rfid_pose.items():
             t = TransformStamped()
             t.header.stamp = self.get_clock().now().to_msg()
             t.header.frame_id = 'map'
             t.child_frame_id = frame_id
-            t.transform.translation.x = pose.pose.position.x
-            t.transform.translation.y = pose.pose.position.y
-            t.transform.translation.z = pose.pose.position.z
-            t.transform.rotation = pose.pose.orientation
+            t.transform.translation.x = pose.pose.pose.position.x
+            t.transform.translation.y = pose.pose.pose.position.y
+            t.transform.translation.z = pose.pose.pose.position.z
+            t.transform.rotation = pose.pose.pose.orientation
             self.tf_broadcaster.sendTransform(t)
 
 def main(args=None):
