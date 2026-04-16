00a7aaf7f3b4facf8c8b04dbb6f91226ca57ab65
JoeCode00 <joe.t.hill00@gmail.com>
2026-04-16 14:31:55 -0400

    attempt at quaternion and position averaging

--- rfid_waypoint_mgr/rfid_waypoint_mgr/rfid_joseph.py
+++ rfid_waypoint_mgr/rfid_waypoint_mgr/rfid_joseph.py
@@ -6,6 +6,9 @@ import tf2_ros
 from std_msgs.msg import String
 from geometry_msgs.msg import TransformStamped, PoseWithCovarianceStamped
 
+import numpy as np
+import math
+
 node_name = 'rfid_joseph'
 qos = 10
 
@@ -41,20 +44,82 @@ class rfid_joseph_node_class(Node):
 
     def rfid_callback(self, input_msg: String):
         id = input_msg.data
-        self.pose_dict[id] = [self.current_pose]
-            
+        if not self.pose_dict[id]:
+            sample_count = 1
+            updated_pose = self.current_pose
+        else:
+            old_pose = self.pose_dict[id][0]
+            sample_count = self.pose_dict[id][1] + 1
+            updated_pose = self.average_pose(old_pose, self.current_pose, sample_count)
+        
+        self.pose_dict[id] = [updated_pose, sample_count]
+
     def pose_callback(self, input_msg: PoseWithCovarianceStamped):
         # self.get_logger().info(f'Input: {str(input_msg)}')
         self.current_pose = input_msg
 
-    def make_pose(self) -> PoseWithCovarianceStamped:
+    def make_pose(self, px=0, py=0, pz=0, qx=0, qy=0, qz=0, qw=1) -> PoseWithCovarianceStamped:
         rfid_pose = PoseWithCovarianceStamped()
         rfid_pose.header.frame_id = 'map'
-        rfid_pose.pose.pose.position.x = 0.0
-        rfid_pose.pose.pose.position.y = 0.0
-        rfid_pose.pose.pose.position.z = 0.0
-        rfid_pose.pose.pose.orientation.w = 1.0
+        rfid_pose.pose.pose.position.x = float(px)
+        rfid_pose.pose.pose.position.y = float(py)
+        rfid_pose.pose.pose.position.z = float(pz)
+        rfid_pose.pose.pose.orientation.x = float(qx)
+        rfid_pose.pose.pose.orientation.y = float(qy)
+        rfid_pose.pose.pose.orientation.z = float(qz)
+        rfid_pose.pose.pose.orientation.w = float(qw)
         return rfid_pose
+    
+    import numpy as np
+
+    def unit_vector(self, data, axis=None, out=None) -> np.ndarray:
+        """Return ndarray normalized by length, i.e. Euclidean norm, along axis.
+        """
+        if out is None:
+            data = np.array(data, dtype=np.float64, copy=True)
+            if data.ndim == 1:
+                data /= math.sqrt(np.dot(data, data))
+                return data
+        else:
+            if out is not data:
+                out[:] = np.array(data, copy=False)
+            data = out
+        length = np.atleast_1d(np.sum(data*data, axis))
+        np.sqrt(length, length)
+        if axis is not None:
+            length = np.expand_dims(length, axis)
+        data /= length
+        if out is None:
+            return data
+
+    def quaternion_slerp(self, quat0, quat1, mu) -> np.ndarray:
+        """Return spherical linear interpolation between two quaternions
+        @param quat0: quaternion (4x1 numpy array)
+        @param quat1: quaternion (4x1 numpy array)
+        @param levels: number of levels for interpolation
+        @return all_q: all interpolated quaternions (list of quaternions)
+        """
+        q0 = self.unit_vector(quat0[:4])
+        q1 = self.unit_vector(quat1[:4])
+        d = np.dot(q0.flatten(), q1.flatten())
+        angle = math.acos(d)
+
+        q_slerp = (np.sin((1 - mu) * angle) / np.sin(angle)) * q0 + (np.sin(mu * angle) / np.sin(angle)) * q1
+
+        return q_slerp
+
+    def average_pose(self, old_pose:PoseWithCovarianceStamped, new_pose:PoseWithCovarianceStamped, sample_count:int|float):
+        mu = 1/sample_count
+        p_old = [old_pose.pose.pose.position.x, old_pose.pose.pose.position.y, old_pose.pose.pose.position.z]
+        p_new = [new_pose.pose.pose.position.x, new_pose.pose.pose.position.y, new_pose.pose.pose.position.z]
+        p_updated = (p_new-p_old)*mu+p_old
+
+        quat_old = np.array([old_pose.pose.pose.orientation.x, old_pose.pose.pose.orientation.y, old_pose.pose.pose.orientation.z, old_pose.pose.pose.orientation.w]).transpose
+        quat_new = np.array([new_pose.pose.pose.orientation.x, new_pose.pose.pose.orientation.y, new_pose.pose.pose.orientation.z, new_pose.pose.pose.orientation.w]).transpose
+        
+        
+        q_updated = self.quaternion_slerp(quat_old, quat_new, mu).flatten().tolist()
+        return self.make_pose(px=p_updated[0], py=p_updated[1], pz=p_updated[2], qx=q_updated[0], qy=q_updated[1], qz=q_updated[2], qw=q_updated[3])
 
     def broadcast_poses(self):
         # for frame_id, pose in self.rfid_pose.items():
